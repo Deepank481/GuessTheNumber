df63e8fcfd87f8e422a603a1603918908600ff65
Deepank481 <pant.deepank045@gmail.com>
2024-01-21 15:31:56 +0530

    21 Jan - Project Completed

--- script.js
+++ script.js
@@ -3,10 +3,12 @@
 const range = 20;
 
 function chooseNumberToGuess(range) {
-  return Math.floor(Math.random() * range + 1);
+  return Math.floor(Math.random() * range + 1); //range can be set between 1 to range
 }
 
 let numberChoosen = chooseNumberToGuess(range);
+
+//Imortant DOM element to be manipulated
 
 const computerNumber = document.getElementById("computer-number");
 
@@ -20,16 +22,28 @@ const userScore = document.getElementById("score");
 
 const gameResetBtn = document.getElementById("game-reset-btn");
 
-let score = 20;
+const highScoreElement = document.getElementById("high-score");
+
+const gameInstruction = document.getElementById("game-instruction");
+gameInstruction.innerText = `Guess a number between 1 and ${range}:`;
+
+//Score Declaration and high score declaration
+
+let score = range;
+let highScore = 0;
 
 userScore.innerText = score;
 
+//Adding event to the buttons
 submitGuessBtn.addEventListener("click", () => {
   checkUserGuess(userGuess.value);
 });
 
 gameResetBtn.addEventListener("click", resetGame);
 
+//Functions to be invoked when events are given by the user
+
+//Function to check the user guess
 function checkUserGuess(number) {
   guessedNumber = parseInt(number);
   if (guessedNumber === NaN) {
@@ -44,20 +58,29 @@ function checkUserGuess(number) {
       } else {
         showUserMessage.innerText = "Too low";
       }
-      score -= 1;
-      userScore.innerText = score;
     } else {
       showUserMessage.innerText = "You Guessed it Right!!!";
       computerNumber.innerText = numberChoosen;
+      submitGuessBtn.disabled = true;
+      userGuess.disabled = true;
     }
+    score -= 1;
+    userScore.innerText = score;
   }
 }
 
+//Function to reset the game
 function resetGame() {
+  if (score > highScore) {
+    highScore = score;
+    highScoreElement.innerText = highScore;
+  }
   score = 20;
   userScore.innerText = score;
   computerNumber.innerText = "?";
   userGuess.value = "";
   numberChoosen = chooseNumberToGuess(range);
   showUserMessage.innerText = "";
+  submitGuessBtn.disabled = false;
+  userGuess.disabled = false;
 }
