673c44a67a0c064234eae167203e2d1afa89a3a4
Deepank481 <pant.deepank045@gmail.com>
2024-01-21 12:03:22 +0530

    21 Jan commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//Computer Selects a Random number Between 1 and 20
+
+const numberChoosen = Math.floor(Math.random() * 20 + 1);
+
+const computerNumber = document.getElementById("computer-number");
+
+const userGuess = document.getElementById("user-guess");
+
+const showUserMessage = document.getElementById("message");
+
+const submitGuessBtn = document.getElementById("submit-guess-btn");
+
+submitGuessBtn.addEventListener("click", () => {
+  checkUserGuess(userGuess.textContent);
+});
+
+function checkUserGuess(guessedNumber) {
+  console.log(guessedNumber);
+  if (guessedNumber > 20 || guessedNumber < 1) {
+    showUserMessage.innerText =
+      "Invalid Input - The number should be between 1 and 20";
+  }
+}
