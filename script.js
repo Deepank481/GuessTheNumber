6088ab06f7e83c7980249434b310f6fbc48290b6
Deepank481 <pant.deepank045@gmail.com>
2024-01-21 12:59:52 +0530

    21 jan commit-2

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 //Computer Selects a Random number Between 1 and 20
 
-const numberChoosen = Math.floor(Math.random() * 20 + 1);
+const range = 20;
+
+function chooseNumberToGuess(range) {
+  return Math.floor(Math.random() * range + 1);
+}
+
+let numberChoosen = chooseNumberToGuess(range);
 
 const computerNumber = document.getElementById("computer-number");
 
@@ -10,13 +16,48 @@ const showUserMessage = document.getElementById("message");
 
 const submitGuessBtn = document.getElementById("submit-guess-btn");
 
+const userScore = document.getElementById("score");
+
+const gameResetBtn = document.getElementById("game-reset-btn");
+
+let score = 20;
+
+userScore.innerText = score;
+
 submitGuessBtn.addEventListener("click", () => {
   checkUserGuess(userGuess.value);
 });
 
-function checkUserGuess(guessedNumber) {
-  if (guessedNumber > 20 || guessedNumber < 1) {
+gameResetBtn.addEventListener("click", resetGame);
+
+function checkUserGuess(number) {
+  guessedNumber = parseInt(number);
+  if (guessedNumber === NaN) {
+    howUserMessage.innerText = "Invalid Input - Not a valid number";
+  } else if (guessedNumber > 20 || guessedNumber < 1) {
     showUserMessage.innerText =
       "Invalid Input - The number should be between 1 and 20";
+  } else {
+    if (guessedNumber !== numberChoosen) {
+      if (guessedNumber > numberChoosen) {
+        showUserMessage.innerText = "Too High";
+      } else {
+        showUserMessage.innerText = "Too low";
+      }
+      score -= 1;
+      userScore.innerText = score;
+    } else {
+      showUserMessage.innerText = "You Guessed it Right!!!";
+      computerNumber.innerText = numberChoosen;
+    }
   }
+}
+
+function resetGame() {
+  score = 20;
+  userScore.innerText = score;
+  computerNumber.innerText = "?";
+  userGuess.value = "";
+  numberChoosen = chooseNumberToGuess(range);
+  showUserMessage.innerText = "";
 }
